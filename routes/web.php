ccd84afcd9fc15bb72ea422f93c35c80708c132e
seyedrezabazyar <seyedrezabazyar@gmail.com>
2022-05-21 17:29:36 +0430

    models ea7

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('create/category', function () {
+    // $created_category = Category::create([
+    //     'title' => 'test 2 title',
+    //     'slug' => 'test-2-title'
+    // ]);
+    // dd($created_category);
+
+    // dd(Category::find(1));
+
+    // dd(Category::where(['slug' => 'test-title'])->first());
+
+    Category::find(1)->update([
+        'title' => '7learn'
+    ]);
 });
