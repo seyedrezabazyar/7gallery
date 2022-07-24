68eb3f906cf6f1be89155e726c91cf450f534519
seyedrezabazyar <seyedrezabazyar@gmail.com>
2022-07-24 12:41:28 +0430

    pagination implemented

--- app/Http/Controllers/Admin/CategoriesController.php
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -31,7 +31,8 @@ class CategoriesController extends Controller
 
     public function all()
     {
-        $categories = Category::all();
+        $categories = Category::paginate(10);
+
         return view('admin.categories.all', compact('categories'));
     }
 }
